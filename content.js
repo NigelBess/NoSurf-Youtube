e017966bd5f6512541e51e851c4ab4639c854531
NigelBess <nigelbess@gmail.com>
2019-05-24 11:35:43 -0700

    v 1.1

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ console.log('1');
 clean(true);
 function clean(repeat)
 {
-	$("#related").remove();
+	$('#related').remove();
+	$('#compainion').remove();
+	$('#secondary').remove();
 	if (repeat)
 	{
 		setTimeout(clean(false),1000);
